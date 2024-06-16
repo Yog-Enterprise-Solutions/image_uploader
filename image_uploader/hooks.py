ca26996880fa41990c55da85f347c7fb4c51c172
yashpataliya01 <yashpataliya01@gmail.com>
2024-06-16 10:23:07 +0530

    folder

--- image_uploader/hooks.py
+++ image_uploader/hooks.py
@@ -122,13 +122,11 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+	"Lead": {
+		"on_update": "image_uploader.override.lead.create_folders",
+	}
+}
 
 # Scheduled Tasks
 # ---------------
