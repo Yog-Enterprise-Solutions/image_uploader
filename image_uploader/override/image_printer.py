360a875c4ad002dd827e8450f785b0889c60319f
yashpataliya01 <yashpataliya01@gmail.com>
2024-06-19 15:10:26 +0530

    final commit

--- image_uploader/override/image_printer.py
+++ image_uploader/override/image_printer.py
@@ -1,8 +1,10 @@
-import frappe 
+import frappe
+from frappe.utils import get_site_name
 
 def doc_link(doc,methods=None):
     name = doc.name
-
-    file_url = f"https://yash.tranqwality.com/api/method/frappe.utils.print_format.download_pdf?doctype=image%20printer&name={name}&format=FIle%20Print&no_letterhead=1&letterhead=No%20Letterhead&settings=%7B%7D&_lang=en"
+    site_name = get_site_name(frappe.local.request.host)
+    frappe.msgprint("site_name")
+    file_url = f"https://{site_name}/api/method/frappe.utils.print_format.download_pdf?doctype=image%20printer&name={name}&format=FIle%20Print&no_letterhead=1&letterhead=No%20Letterhead&settings=%7B%7D&_lang=en"
 
     doc.file_link = file_url
